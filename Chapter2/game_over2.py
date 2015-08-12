bdf4ef270ae00e97e4bf33d03a8c66c3e8c8fa55
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-08-12 22:08:03 +0100

    Chapter 2 start
    messing about with different string formats.

---
+++ Chapter2/game_over2.py
@@ -0,0 +1,22 @@
+#game over - version 2
+#demonstrates the use of quotes in strings.
+print("program 'game over' 2.0")
+print("same", "message", "as before")
+print("just"
+      "a bit"
+      "bigger")
+print("here", end= " ")
+print("it is...")
+
+print(
+        """
+  ________    _____      _____  ___________ ____________   _________________________._.
+ /  _____/   /  _  \    /     \ \_   _____/ \_____  \   \ /   /\_   _____/\______   \ |
+/   \  ___  /  /_\  \  /  \ /  \ |    __)_   /   |   \   Y   /  |    __)_  |       _/ |
+\    \_\  \/    |    \/    Y    \|        \ /    |    \     /   |        \ |    |   \\|
+ \______  /\____|__  /\____|__  /_______  / \_______  /\___/   /_______  / |____|_  /__
+        \/         \/         \/        \/          \/                 \/         \/ \/
+
+        """)
+
+input("\npress enter to exit")
