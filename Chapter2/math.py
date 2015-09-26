8276a1aaa2a5b94e377306cddeac8e0d66bdbbc2
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-09-26 18:36:00 +0100

    Math functions and String variable
    Math functions and String variable

---
+++ Chapter2/math.py
@@ -0,0 +1,8 @@
+print("5 + 7 = ", 5 + 7)
+print("5667 x 56799 = ", 5667 * 56799)
+print("7 / 3 = ", 7 / 3)
+print("int division: 7 // 3 = ", 7 // 3)
+print("67 % 3 = ", 67 % 3)
+print ("12 - 2 = ", 12 - 2)
+
+input("press enter to continue")
---
+++ Chapter2/variables.py
@@ -0,0 +1,6 @@
+pc_name = "Compy"
+name = input("Hi, what is your name? ")
+print("Hi", name, "my name is", pc_name)
+intput("\n\nPress the enter key to exit")
+
+
