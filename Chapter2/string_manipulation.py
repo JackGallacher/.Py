93baae2808adfc5babfa1caf1d1398c30202e828
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-09-27 20:39:25 +0100

    String manipulation
    String manipulation

---
+++ Chapter2/string_manipulation.py
@@ -0,0 +1,8 @@
+quote = "This is my quote!"
+print(quote + "\n")
+print("text in upper case: " + quote.upper())
+print("text in lower case: " + quote.lower())
+print("text as title: " + quote.title())
+print("text with replaced words: " + quote.replace("quote!", "REPLACMENT TEXT HAHAHA!!"))
+print("text with first letter cap: " + quote.capitalize())
+print("stripped text: " + quote.strip())
